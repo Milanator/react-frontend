825b52e34260a3c5c68c853decc720ebc8b16a60
MeikeKrohn <mk261@hdm-stuttgart.de>
2018-11-12 13:23:10 +0200

    Added basic login-screen and bootstrap

---
+++ src/Login.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h1 className="form-heading">login Form</h1>
+                <div className="login-form">
+                    <div className="main-div">
+                        <div className="panel">
+                            <h2>Login</h2>
+                            <p>Please enter your email and password</p>
+                        </div>
+                        
+                        <form id="Login">
+                            <div className="form-group">
+                                <input type="email" className="form-control" id="inputEmail" placeholder="Email Address" />
+                            </div>
+
+                            <div className="form-group">
+                                <input type="password" className="form-control" id="inputPassword" placeholder="Password" />
+                            </div>
+                                    
+                            <button type="submit" className="btn btn-primary">Login</button>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+                );
+            }
+        }
+        
+export default Login;
