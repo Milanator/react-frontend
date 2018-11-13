2f5dccc6dfef8322c42b9627ae2665c0fbf02f65
MeikeKrohn <mk261@hdm-stuttgart.de>
2018-11-13 08:12:51 +0200

    Added Application Logo

--- src/Login.js
+++ src/Login.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
+import logo from "./Logo.png";
 
 class Login extends Component {
     render() {
         return (
             <div className="container">
+
                 <h1 className="form-heading">login Form</h1>
+                
                 <div className="login-form">
                     <div className="main-div">
+
+                        <div className="login-image">
+                            <img src={logo} width="70px"/>
+                        </div>
+
                         <div className="panel">
                             <h2>Login</h2>
                             <p>Please enter your email and password</p>
                         </div>
-                        
+
                         <form id="Login">
                             <div className="form-group">
                                 <input type="email" className="form-control" id="inputEmail" placeholder="Email Address" />
@@ -20,15 +28,15 @@ class Login extends Component {
                             <div className="form-group">
                                 <input type="password" className="form-control" id="inputPassword" placeholder="Password" />
                             </div>
-                                    
+
                             <button type="submit" className="btn btn-primary">Login</button>
 
                         </form>
                     </div>
                 </div>
             </div>
-                );
-            }
-        }
-        
+        );
+    }
+}
+
 export default Login;
