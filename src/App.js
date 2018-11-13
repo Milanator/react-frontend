27e0195fef90fc8adb1878bd31a08aa9e875c6ff
Peter Blstak <blstak11peter@gmail.com>
2018-11-13 13:45:50 +0200

    Added FilmCard.js ,FilmCard.css , implemented to APP.js and installed google-material-icons trough NPM

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 import Login from './components/Login.js';
+import FilmCard from './components/FilmCard.js';
 import { Route, Switch } from 'react-router-dom'
+import './css/FilmCard.css'
+
 
 class App extends Component {
   render() {
@@ -9,6 +12,7 @@ class App extends Component {
       <div className={'app-routes'}>
 		  <Switch>
 			  <Route path={'/login'} component={Login} />
+			  <Route path={'/filmcard'} component={FilmCard} />
 		  </Switch>
 	  </div>
     );
---
+++ src/components/FilmCard.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import MaterialIcon from 'react-google-material-icons'
+
+
+let ourApiUrl = 'http://localhost:3000/';
+let baseUrl = 'http://localhost:3001/';
+
+class FilmCard extends Component {
+
+
+    render() {
+        return (
+            <div className="card">
+                <img src="#" />
+
+                    <div className="marks">
+
+                        <MaterialIcon icon="star_half" size={25}/>
+                        <p id="rating">50%</p>
+                        <a className=" image"> <MaterialIcon icon="check_box" size={25}/></a>
+                        <a className=" image"><MaterialIcon icon="bookmark_border" size={25}/></a>
+
+                    </div>
+                    <h3><a href="#">FILM NAME</a></h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Nunc feugiat, eros ut euismod ultrices, nisi ligula
+                        sollicitudin eros, a posuere felis massa vel odio.
+                        Vivamus vel elementum lacus<a href="#"> ...</a>
+                    </p>
+                    <p>language: EN</p>
+            </div>
+        );
+    }
+}
+
+export default FilmCard;
