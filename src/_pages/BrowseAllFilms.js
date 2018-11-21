f3f44efcb5557751dd53964541d8d7728a9608b7
MeikeKrohn <mk261@hdm-stuttgart.de>
2018-11-21 13:58:18 +0200

    Created Pagination for BrowseAllFilms

--- src/_pages/BrowseAllFilms.js
+++ src/_pages/BrowseAllFilms.js
@@ -3,40 +3,58 @@ import TopNavigation from '../_components/TopNavigation';
 import axios from 'axios';
 import FilmModal from '../_components/FilmModal';
 import FilterSidebar from '../_components/FilterSidebar';
+import LoadingIndicator from '../_components/LoadingIndicator';
 import '../css/main.css';
+import { Pagination } from 'semantic-ui-react';
+import { movieDbDomain, movieApiKeyPart } from '../_helpers/variable';
 
-var apikey = "e0338266d7945597731b014d7e806075";
-var apiurlparams = "&language=en-US&sort_by=popularity.desc";
-var apiurl = "https://api.themoviedb.org/3/discover/movie?api_key=" + apikey + apiurlparams;
+var apiurlparams = "&language=en-US&sort_by=popularity.desc&page=";
+var apiUrl = movieDbDomain + "3/discover/movie" + movieApiKeyPart + apiurlparams;
+var data;
+var isLoading;
 
 class BrowseAllFilms extends Component {
 	constructor(props) {
 		super(props);
+		isLoading = true;
 
 		this.state = {
-			data: []
+			data: [],
+			activePage: 1
 		};
+
+		this.handlePaginationChange = this.handlePaginationChange.bind(this);
 	}
 
 	componentDidMount() {
-
-		axios.get(apiurl).then(res => {
+		axios.get(apiUrl + this.state.activePage).then(res => {
+			isLoading = false;
 			const data = res.data.results;
 			this.setState({ data });
 		});
 	}
 
+	handlePaginationChange = (e, { activePage }) => {
+		isLoading = true;
+		this.setState({ activePage }, () => {
+			axios.get(apiUrl + this.state.activePage).then(res => {
+				isLoading = false;
+				data = res.data.results;
+				this.setState({ data });
+			})
+		});
+
+	}
+
 	render() {
-
-		if( this.state.data.lenght == 0 ){
-
-			return <div>Loading</div>
-		} else{
-
+		if (isLoading == true) {
+			return <div><LoadingIndicator /></div>
+		} else {
 			return (
 				<div>
 					<TopNavigation />
 					<FilterSidebar />
+
 					{this.state.data.map(film => (
 						<FilmModal
 							id={film.id}
@@ -47,6 +65,9 @@ class BrowseAllFilms extends Component {
 							original_language={film.original_language}
 						/>
 					))}
+					<div className="pagination-component">
+						<Pagination activePage={this.state.activePage} totalPages={20} onPageChange={this.handlePaginationChange} />
+					</div>
 				</div>
 			);
 		}
