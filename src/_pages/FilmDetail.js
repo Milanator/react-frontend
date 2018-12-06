7cf922e33d675d50abf956f5e3985633e01a7ff5
Blstyy <blstak11peter@gmail.com>
2018-12-06 16:18:05 +0200

    Added footer and gallery

--- src/_pages/FilmDetail.js
+++ src/_pages/FilmDetail.js
@@ -8,43 +8,44 @@ import ListButtons from "../_components/ListButtons";
 import"../_helpers/method"
 import ImageSlider from "../_components/ImageSlider";
 import {setFilmGenre, setMyListToMovie} from "../_helpers/method";
-import FilmCardSlider from "../_components/FilmCardSlider";
 import ImageLightbox from "../_components/ImageLightbox";
+import { formatMoney}  from "../_helpers/method"
+import Footer from "../_components/Footer";
 
 let apiUrl = movieDbDomain + 'movie';
 var imageurl = "https://api.themoviedb.org/3/movie/";
-class FilmDetail extends React.Component {
+    class FilmDetail extends React.Component {
 
-    constructor(props) {
+        constructor(props) {
 
-        super(props);
+            super(props);
 
-        this.state = {
-            filmId: this.props.match.params.id,
-            filmData: [],
-            inSeenList:"",
-            userLists: [],
-            images:[],
+            this.state = {
+                filmId: this.props.match.params.id,
+                filmData: [],
+                inSeenList:"",
+                userLists: [],
+                images:[],
 
+                }
         }
-       }
 
-    componentDidMount() {
+        componentDidMount() {
 
-        let filmId = this.state.filmId;
-        let url = apiUrl + '/' + filmId + movieApiKeyPart;
+            let filmId = this.state.filmId;
+            let url = apiUrl + '/' + filmId + movieApiKeyPart;
 
-        axios.get(url + "&append_to_response=videos")
-            .then(res => {
-                console.log(res);
-                this.setState({filmData: res.data})
-            }).catch(err => {
-            console.log(err);
-        });
+            axios.get(url + "&append_to_response=videos")
+                .then(res => {
+                    console.log(res);
+                    this.setState({filmData: res.data})
+                }).catch(err => {
+                console.log(err);
+                });
 
-        let that = this;
-        let seenList, watchList, userLists, myListMovies, genresArray,images;
-        let { userId } = this.state;
+            let  watchList, userLists, myListMovies, genresArray;
+
+            let { userId } = this.state;
         axios.all([
             //images
             axios.get (imageurl +filmId+"/images?api_key=" + apikey)
@@ -53,48 +54,48 @@ class FilmDetail extends React.Component {
                     this.setState({images: res.data})
                 }).catch(err => {
                 console.log(err);
-            }),
+                }),
 
             // watchlist
             axios.get(ourApiUrl + 'watchlist/user/' + userId).then(res => {
 
                 watchList = res.data;
-            }).catch((error) => { console.log( error ); }),
+                    }).catch((error) => { console.log( error ); }),
 
             // get all names and idies lists
             axios.get(ourApiUrl + "mylist/user/"+userId+"/category").then((res) => {
 
                 userLists = res.data;
-            }).catch((error) => { console.log( error ); }),
+                     }).catch((error) => { console.log( error ); }),
 
             // get all names and idies lists
             axios.get(ourApiUrl + "mylist/user/"+userId+"/all").then((res) => {
 
                 myListMovies = res.data;
                 // myListMovies = setMyListToMovie(this.state.films,myListMovies);
-            }).catch((error) => {
+                    }).catch((error) => {
 
-                console.log( error );
-            })
+                    console.log( error );
+                    })
 
-        ]).then(() => {
+            ]).then(() => {
 
             let films = setFilmGenre(genresArray, this.state.films);
             films = setMyListToMovie(films,myListMovies);
 
             this.setState({ films:films,userLists:userLists,genres: genresArray });
-        }).then(() => {
-            this.setState({ isLoading: false });
-        }).catch((err) => {
-            console.log(err);
-        });
+                 }).then(() => {
+                    this.setState({ isLoading: false });
+                 }).catch((err) => {
+                    console.log(err);
+                    });
 
-    }
+        }
 
     render() {
         const {filmId, filmData, inSeenList, inWatchList,userLists,images} = this.state;
 
-        if (filmId == 0 ||filmData ==0||images==0) {
+        if ( filmId == 0 || filmData == 0 || images== 0 ) {
             return <LoadingIndicator/>;
         } else {
                 return(
@@ -105,22 +106,23 @@ class FilmDetail extends React.Component {
                             <img className="filmdetailimg" src={"https://image.tmdb.org/t/p/w342/" + filmData.poster_path}/>
                             <h1>{filmData.original_title}</h1>
 
-                                <ListButtons
-                                    id={filmData.filmId}
-                                    rating={filmData.vote_average }
-                                    inSeenList={inSeenList}
-                                    inWatchList={inWatchList}
-                                    sendWatchSeen={this.setWatchSeen}
-                                    userLists={userLists}
-                                 />
-
-
                             <p>{filmData.overview}</p>
-                            <p>Original language : {filmData.original_language}</p>
-                                {filmData.videos.results[0] && (
-                            <iframe src={"https://www.youtube.com/embed/" + filmData.videos.results[0].key} width={600} height={300}>
-                            </iframe>)}
+                            <p>Original language: {filmData.original_language}</p>
 
+                            {filmData.videos.results[0] && (
+                                <iframe src={"https://www.youtube.com/embed/" +
+                                filmData.videos.results[0].key} width={600} height={300}>
+                                </iframe>
+                            )}
+
+                            <ListButtons
+                                id={filmData.filmId}
+                                rating={filmData.vote_average }
+                                inSeenList={inSeenList}
+                                inWatchList={inWatchList}
+                                sendWatchSeen={this.setWatchSeen}
+                                userLists={userLists}
+                            />
                             <table className="table">
                                 <tr>
                                     <th>Release year</th>
@@ -141,24 +143,26 @@ class FilmDetail extends React.Component {
                                         </tr>
                                         <tr>
                                             <th>Vote count</th>
-                                            <td>{filmData.vote_count}</td>
+                                            <td>{formatMoney(filmData.vote_count, 0)}</td>
                                         </tr>
                                         <tr>
                                             <th>Budget</th>
-                                            <td>{filmData.budget}$</td>
+                                            <td>{formatMoney(filmData.budget, 0)}$</td>
                                         </tr>
                                         <tr>
                                             <th>Revenue</th>
-                                            <td>{filmData.revenue}$</td>
+                                            <td>{formatMoney(filmData.revenue,0)}$</td>
                                         </tr>
                                 </tbody>
                             </table>
+
                             <ImageSlider
-                                images={images.backdrops}
-                                />
+                                images={images.backdrops} />
+
                             <ImageLightbox image = {images.backdrops} />
 
                         </div>
+                        <Footer/>
                     </div>
                 );
             }
@@ -166,6 +170,4 @@ class FilmDetail extends React.Component {
         }
 
 }
-
-
-export default FilmDetail;
+    export default FilmDetail;
