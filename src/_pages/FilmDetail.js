dd3519ba46a55726c8ae9643abeab75396058e43
Blstyy <blstak11peter@gmail.com>
2018-12-10 09:12:57 +0200

    footer

--- src/_pages/FilmDetail.js
+++ src/_pages/FilmDetail.js
@@ -8,184 +8,205 @@ import LoadingIndicator from "../_components/LoadingIndicator";
 import TopNavigation from "../_components/TopNavigation";
 import ListButtons from "../_components/ListButtons";
 import ImageSlider from "../_components/ImageSlider";
-import {setFilmGenre, setMyListToMovie} from "../_helpers/method";
-import FilmCardSlider from "../_components/FilmCardSlider";
 import ImageLightbox from "../_components/ImageLightbox";
+import CommentBlock from "../_components/CommentBlock";
 
 import"../_helpers/method"
 import "../css/FilmDetail.css"
+import Footer from "../_components/Footer";
 
 let apiUrl = movieDbDomain + 'movie';
-var imageurl = "https://api.themoviedb.org/3/movie/";
+let imageUrl = "https://api.themoviedb.org/3/movie/";
+
 class FilmDetail extends React.Component {
 
-        constructor(props) {
+    constructor(props) {
 
-            super(props);
+        super(props);
+
+        let user = JSON.parse(localStorage.getItem('user'));
+        let userId = atob(user.id);
+        let profilePicture = atob(user.profilePicture);
+        let userName = atob(user.name);
 
         this.state = {
-            filmId: this.props.match.params.id,
+            movieId: this.props.match.params.id,
             filmData: [],
             inSeenList:"",
             userLists: [],
             images:[],
-
+            userId: userId,
+            userName: userName,
+            profilePicture: profilePicture,
+            movieInMyLists: [],
+            comments: [],
+            isLoading: true,
+            DBoffest: 1
         }
-       }
 
-        componentDidMount() {
+        this.addToMyList = this.addToMyList.bind(this);
+    }
 
-        let filmId = this.state.filmId;
-        let url = apiUrl + '/' + filmId + movieApiKeyPart;
+    componentDidMount() {
 
-        axios.get(url + "&append_to_response=videos")
-            .then(res => {
-                console.log(res);
-                this.setState({filmData: res.data})
-            }).catch(err => {
-            console.log(err);
-        });
+        let movieId = this.state.movieId;
+        let url = apiUrl + '/' + movieId + movieApiKeyPart;
+        let userLists, myListMovies, genresArray,comments;
+        let {userId,DBoffest} = this.state;
 
-        let that = this;
-        let seenList, watchList, userLists, myListMovies, genresArray,images;
-        let { userId } = this.state;
         axios.all([
+
+            // film with video
+            axios.get(url + "&append_to_response=videos")
+                .then(res => {
+
+                    let filmData = [];
+                    filmData.push(res.data);
+                    this.setState({filmData: filmData})
+                }).catch(err => { console.log(err); }),
+
             //images
             axios.get (imageUrl +movieId+"/images?api_key=" + apikey)
                 .then(res => {
                     this.setState({images: res.data})
-                }).catch(err => {
-                console.log(err);
-            }),
-
-            // watchlist
-            axios.get(ourApiUrl + 'watchlist/user/' + userId).then(res => {
-
-                watchList = res.data;
-            }).catch((error) => { console.log( error ); }),
+                }).catch(err => { console.log(err); }),
 
             // get all names and idies lists
             axios.get(ourApiUrl + "mylist/user/"+userId+"/category").then((res) => {
 
                 userLists = res.data;
-                     }).catch((error) => { console.log( error ); }),
+            }).catch((error) => { console.log( error ); }),
 
             // get all names and idies lists
             axios.get(ourApiUrl + "mylist/user/"+userId+"/all").then((res) => {
 
                 myListMovies = res.data;
-                // myListMovies = setMyListToMovie(this.state.films,myListMovies);
-            }).catch((error) => {
+            }).catch((error) => { console.log( error ); }),
 
-                console.log( error );
-            })
+            // get comments by movie id
+            axios.get(ourApiUrl + "comment/movie/"+movieId+"/"+DBoffest).then((res) => {
+
+                comments = res.data;
+            }).catch((error) => { console.log( error ); })
+
 
         ]).then(() => {
 
             let films = setMyListToMovie(this.state.filmData,myListMovies);
             let filmListIdies = films[0].inMyLists;
 
-            this.setState({ films:films,userLists:userLists,genres: genresArray });
+            this.setState({ films:films,userLists:userLists,genres:genresArray,movieInMyLists:filmListIdies,comments:comments.reverse() });
         }).then(() => {
             this.setState({ isLoading: false });
         }).catch((err) => {
             console.log(err);
         });
 
-        }
+    }
 
-	addToMyList = (event) => {
+    addToMyList = (event) => {
 
-		addMyList(event, this, 0)
-	}
+        addMyList(event, this, 0)
+    }
 
     render() {
 
-        if (filmId == 0 ||filmData ==0||images==0) {
+        const {movieId,userLists,images,userId,profilePicture,userName,comments,isLoading,DBoffest} = this.state;
+        let {filmData} = this.state;
+        filmData = filmData[0];
+
+        if ( movieId == 0 || filmData == 0 || images == 0 || isLoading ) {
             return <LoadingIndicator/>;
         } else {
-                return(
-                    <div>
-                        <TopNavigation/>
-                        <div className={"content"}>
-                            <img className="filmdetailimg" src={"https://image.tmdb.org/t/p/w342/" + filmData.poster_path}/>
+            return(
+                <div>
+                    <TopNavigation/>
+                    <div className={"content"}>
+                        <div className={'main-detail clearfix'}>
+                            <img className={"filmdetailimg"} src={"https://image.tmdb.org/t/p/w342/" + filmData.poster_path}/>
                             <h1>{filmData.original_title}</h1>
 
-                                <ListButtons
-                                    id={filmData.filmId}
-                                    rating={filmData.vote_average }
-                                    inSeenList={inSeenList}
-                                    inWatchList={inWatchList}
-                                    sendWatchSeen={this.setWatchSeen}
-                                    userLists={userLists}
-                                 />
+                            <ListButtons
+                                userLists={userLists}
+                                rating={filmData.vote_average}
+                                movieInMyLists={this.state.movieInMyLists}
+                                addToMyList={this.addToMyList}
+                                userId={userId}
+                                movieId={filmData.id}
+                                poster_path={filmData.poster_path}
+                                title={filmData.title}
+                                overview={filmData.overview}
+                                original_language={filmData.original_language}
+                                genres={filmData.genres}
+                            />
 
-                            {filmData.videos.results[0] && (
-                                <iframe src={"https://www.youtube.com/embed/" +
-                                filmData.videos.results[0].key} width={600} height={300}>
-                                </iframe>
-                            )}
 
                             <p>{filmData.overview}</p>
-                            <p>Original language : {filmData.original_language}</p>
-                                {filmData.videos.results[0] && (
-                            <iframe src={"https://www.youtube.com/embed/" + filmData.videos.results[0].key} width={600} height={300}>
-                            </iframe>)}
-
-                            <table className="table">
-                                <tr>
-                                    <th>Release year</th>
-                                    <td>{filmData.release_date}</td>
-                                </tr>
-                                     <tbody>
-                                        <tr>
-                                            <th>Runtime</th>
-                                            <td>{filmData.runtime} minutes</td>
-                                        </tr>
-                                        <tr>
-                                            <th>Status</th>
-                                            <td>{filmData.status}</td>
-                                        </tr>
-                                        <tr>
-                                            <th>Average vote</th>
-                                            <td>{filmData.vote_average *10}%</td>
-                                        </tr>
-                                        <tr>
-                                            <th>Vote count</th>
-                                            <td>{formatMoney(filmData.vote_count, 0)}</td>
-                                        </tr>
-                                        <tr>
-                                            <th>Budget</th>
-                                            <td>{formatMoney(filmData.budget, 0)}$</td>
-                                        </tr>
-                                        <tr>
-                                            <th>Revenue</th>
-                                            <td>{formatMoney(filmData.revenue,0)}$</td>
-                                        </tr>
-                                </tbody>
-                            </table>
-
-                            <ImageSlider
-                                images={images.backdrops} />
-
-                            <ImageLightbox image = {images.backdrops} />
-
-							<CommentBlock
-								movieId={movieId}
-								userId={userId}
-								profilePicture={profilePicture}
-								userName={userName}
-								comments={comments}
-								DBoffest={DBoffest}
-							/>
-
+                            <p>Original language: {filmData.original_language}</p>
+                            {filmData.videos.results[0] && (
+                                <iframe src={"https://www.youtube.com/embed/" + filmData.videos.results[0].key} width={600} height={300}>
+                                </iframe>
+                            )}
                         </div>
-                        <Footer/>
-                    </div>
-                );
-            }
 
+                        <ImageSlider
+                            images={images.backdrops}
+                        />
+                        <ImageLightbox
+                            image={images.backdrops}
+                        />
+
+                        <table className={"table"}>
+                            <tr>
+                                <th>Release year</th>
+                                <td>{filmData.release_date}</td>
+                            </tr>
+                            <tbody>
+                            <tr>
+                                <th>Runtime</th>
+                                <td>{filmData.runtime} minutes</td>
+                            </tr>
+                            <tr>
+                                <th>Status</th>
+                                <td>{filmData.status}</td>
+                            </tr>
+                            <tr>
+                                <th>Average vote</th>
+                                <td>{filmData.vote_average *10}%</td>
+                            </tr>
+                            <tr>
+                                <th>Vote count</th>
+                                <td>{filmData.vote_count}</td>
+                            </tr>
+                            <tr>
+                                <th>Budget</th>
+                                <td>{filmData.budget}$</td>
+                            </tr>
+                            <tr>
+                                <th>Revenue</th>
+                                <td>{filmData.revenue}$</td>
+                            </tr>
+                            </tbody>
+                        </table>
+
+                        <CommentBlock
+                            movieId={movieId}
+                            userId={userId}
+                            profilePicture={profilePicture}
+                            userName={userName}
+                            comments={comments}
+                            DBoffest={DBoffest}
+                        />
+
+                    </div>
+                    <Footer/>
+                </div>
+            );
         }
 
+    }
+
 }
-    export default FilmDetail;
+
+
+export default FilmDetail;
